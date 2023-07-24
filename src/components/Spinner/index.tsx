f76fbfed5ccb8bbaf406c0ea1b119dc0dae956b4
Kate <vesnavved@gmail.com>
2023-07-25 00:52:51 +0300

    add scss, create spinner

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,8 @@
+import "./spinner.scss";
+import React from "react";
+
+export const Spinner: React.FC = () => (
+  <div className="pos-center">
+    <div className="loader"></div>
+  </div>
+);
---
+++ src/enums/routes.enum.ts
@@ -0,0 +1,11 @@
+enum Routes {
+  Index = "/",
+  SignUp = "/sign-up",
+  SignIn = "/sign-in",
+  TripId = "/trip/:tripId",
+  Trip = "/trip",
+  Bookings = "/bookings",
+  Unknown = "*",
+}
+
+export { Routes };
